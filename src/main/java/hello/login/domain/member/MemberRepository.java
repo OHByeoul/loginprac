ad97b05cb8187fcf0e549b676084ab7b02ce9eda
오한별 <suriessay@naver.com>
2022-05-30 20:34:41 +0900

    디비없이 유저 CRUD

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,36 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+    private static Map<Long, Member> repository = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        repository.put(member.getId(),member);
+        return member;
+    }
+    public Member findById(Long id) {
+        return repository.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+        return findAll().stream()
+                .filter(e -> e.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(repository.values());
+    }
+
+    public void clearStore() {
+        repository.clear();
+    }
+}
